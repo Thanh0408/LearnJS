132f617336f81be6afbcf9b1243be9e1fe4ba47a
Thanh0408 <quangthanh.hust@gmail.com>
2022-03-21 21:29:02 +0700

    initial commit

---
+++ Calculate app/script.js
@@ -0,0 +1,13 @@
+function getHistory() {
+    return document.getElementById("history_value").innerText;
+}
+function printHistory(numb) {
+    document.getElementById("history_value").innerText = numb;
+}
+function getOutput() {
+    return document.getElementById("output-value").innerText;
+}
+
+function setHistory() {
+    
+}
